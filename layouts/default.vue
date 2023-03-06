09138b49dc58954339dccacec748a5111500de8a
sheilaazhar <sheila.azhar98@gmail.com>
2023-03-06 17:37:38 +0700

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <main class="max-w-lg min-h-screen mx-auto shadow-md">
+      <slot />
+    </main>
+</template>  
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    runtimeConfig: {
+        public: {
+          apiBase: process.env.NUXT_BASE_URL,
+        },
+      },
+    modules: ['@nuxtjs/tailwindcss']
+})
