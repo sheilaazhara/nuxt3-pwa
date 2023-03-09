0970b042a0a3cebd10105180621b4422b422ad6c
sheilaazhar <sheila.azhar98@gmail.com>
2023-03-09 16:34:20 +0700

    install nuxt pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-          apiBase: process.env.NUXT_BASE_URL,
-        },
-      },
-    modules: ['@nuxtjs/tailwindcss']
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_BASE_URL,
+    },
+  },
+  modules: ['@nuxtjs/tailwindcss','@kevinmarrec/nuxt-pwa'],
+  pwa: {
+    workbox: { enabled: false },
+  },
 })
